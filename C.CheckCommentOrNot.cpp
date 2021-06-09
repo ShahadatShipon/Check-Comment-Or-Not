c5fc3af1ad85215c75c25d40998287f780a7d788
ShahadatShipon <85637341+ShahadatShipon@users.noreply.github.com>
2021-06-09 23:39:41 +0600

    Add files via upload

---
+++ C.CheckCommentOrNot.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s;int c=0;
+    cout<<"Enter any line: ";cin>>s;
+
+    for(int i=0;i<s.size();i++){
+        if(s.at(i)=='/' && s.at(i+1)=='/'){
+            cout<<"This line has a comment."<<endl;
+            c++;
+            break;
+        }
+    }
+    if(c==0){
+        cout<<"This line has not a comment."<<endl;
+    }
+
+
+    return 0;
+}
